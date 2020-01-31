4021c67c35bf13ab1d46a7b9f92d705033a3f005
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-01-31 15:39:13 -0300

    14

---
+++ Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlatformTrigger.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+APlatformTrigger::APlatformTrigger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Box"));
+	if (ensure(TriggerVolume != nullptr))
+	{
+		SetRootComponent(TriggerVolume);
+		MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Trigger Mesh"));
+		MeshComp->SetupAttachment(TriggerVolume);
+	}
+
+}
+
+// Called when the game starts or when spawned
+void APlatformTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APlatformTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/PuzzlePlatforms/PlatformTrigger.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformTrigger.generated.h"
+
+UCLASS()
+class PUZZLEPLATFORMS_API APlatformTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatformTrigger();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Trigger")
+	class UBoxComponent* TriggerVolume;
+	UPROPERTY(VisibleAnywhere, Category="Trigger")
+	class UStaticMeshComponent* MeshComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+};
